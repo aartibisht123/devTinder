e7d4441380352a5828f59c8bca338c4044dfaf28
aartibisht123 <aartbisht@gmail.com>
2025-12-01 13:10:01 +0530

    created an express server

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.use("/hello", (req, res) => {
+    res.send('hello ');
+});
+
+app.use("/", (req, res) => {
+    res.send('hello  aartiii');
+});
+
+app.use("/hii", (req, res) => {
+    res.send('  hii hello  aartiii');
+});
+
+
+
+app.listen(3000, ()=>{
+    console.log('server is running on port 3000')
+});
